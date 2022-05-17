73636177ae374ea9aa407767b6aa43aa04c6da55
disrupted <hi@salomonpopp.me>
2022-05-17 20:56:40 +0200

    feat(color): add light theme

--- lua/one/init.lua
+++ lua/one/init.lua
@@ -1,63 +1,66 @@
 local M = {}
 
--- dark colors
-local colors = {
-    mono_1 = '#abb2bf',
-    mono_2 = '#828997',
-    mono_3 = '#5c6370',
-    mono_4 = '#4b5263',
-    hue_1 = '#56b6c2',
-    hue_2 = '#61afef',
-    hue_3 = '#c678dd',
-    hue_4 = '#98c379',
-    hue_5 = '#e06c75',
-    hue_5_2 = '#be5046',
-    hue_6 = '#d19a66',
-    hue_6_2 = '#e5c07b',
-    syntax_bg = '#24282c',
-    syntax_gutter = '#636d83',
-    syntax_cursor = '#2c323c',
-    syntax_accent = '#528bff',
-    vertsplit = '#181a1f',
-    special_grey = '#3b4048',
-    visual_grey = '#3e4452',
-    pmenu = '#2c323c',
-    term_black = '#282c34',
-    term_blue = '#61afef',
-    term_cyan = '#56b6c2',
-    term_white = '#dcdfe4',
-    term_8 = '#5d677a',
-}
+local colors = {}
 
--- light colors
--- TODO
---[[ local colors = {
-    mono_1 = { '#494b53', 59, 'mono_1' },
-    mono_2 = { '#696c77', 60, 'mono_2' },
-    mono_3 = { '#a0a1a7', 247, 'mono_3' },
-    mono_4 = { '#c2c2c3', 251, 'mono_4' },
-    hue_1 = { '#0184bc', 31, 'hue_1' },
-    hue_2 = { '#4078f2', 69, 'hue_2' },
-    hue_3 = { '#a626a4', 243, 'hue_3' },
-    hue_4 = { '#50a14f', 242, 'hue_4' },
-    hue_5 = { '#e45649', 244, 'hue_5' },
-    hue_5_2 = { '#ca1243', 241, 'hue_5_2' },
-    hue_6 = { '#986801', 94, 'hue_6' },
-    hue_6_2 = { '#c18401', 242, 'hue_6_2' },
-    syntax_bg = { '#fafafa', 231, 'syntax_bg' },
-    syntax_gutter = { '#9e9e9e', 247, 'syntax_gutter' },
-    syntax_cursor = { '#f0f0f0', 255, 'syntax_cursor' },
-    syntax_accent = { '#526fff', 246, 'syntax_accent' },
-    vertsplit = { '#e7e9e1', 254, 'vertsplit' },
-    special_grey = { '#d3d3d3', 252, 'special_grey' },
-    visual_grey = { '#d0d0d0', 252, 'visual_grey' },
-    pmenu = { '#dfdfdf', 254, 'pmenu' },
-    term_black = { '#383a42', 237, 'term_black' },
-    term_blue = { '#0184bc', 31, 'term_blue' },
-    term_cyan = { '#0997b3', 243, 'term_cyan' },
-    term_white = { '#fafafa', 231, 'term_white' },
-    term_8 = { '#4f525e', 240, 'term_8' },
-} ]]
+if vim.o.background == 'dark' then
+    -- dark colors
+    colors = {
+        mono_1 = '#abb2bf',
+        mono_2 = '#828997',
+        mono_3 = '#5c6370',
+        mono_4 = '#4b5263',
+        hue_1 = '#56b6c2',
+        hue_2 = '#61afef',
+        hue_3 = '#c678dd',
+        hue_4 = '#98c379',
+        hue_5 = '#e06c75',
+        hue_5_2 = '#be5046',
+        hue_6 = '#d19a66',
+        hue_6_2 = '#e5c07b',
+        syntax_bg = '#24282c',
+        syntax_gutter = '#636d83',
+        syntax_cursor = '#2c323c',
+        syntax_accent = '#528bff',
+        vertsplit = '#181a1f',
+        special_grey = '#3b4048',
+        visual_grey = '#3e4452',
+        pmenu = '#2c323c',
+        term_black = '#282c34',
+        term_blue = '#61afef',
+        term_cyan = '#56b6c2',
+        term_white = '#dcdfe4',
+        term_8 = '#5d677a',
+    }
+else
+    -- light colors
+    colors = {
+        mono_1 = '#494b53',
+        mono_2 = '#696c77',
+        mono_3 = '#a0a1a7',
+        mono_4 = '#c2c2c3',
+        hue_1 = '#0184bc',
+        hue_2 = '#4078f2',
+        hue_3 = '#a626a4',
+        hue_4 = '#50a14f',
+        hue_5 = '#e45649',
+        hue_5_2 = '#ca1243',
+        hue_6 = '#986801',
+        hue_6_2 = '#c18401',
+        syntax_bg = '#eaeaea',
+        syntax_gutter = '#9e9e9e',
+        syntax_cursor = '#f0f0f0',
+        syntax_accent = '#526fff',
+        vertsplit = '#e7e9e1',
+        special_grey = '#d3d3d3',
+        visual_grey = '#d0d0d0',
+        pmenu = '#dfdfdf',
+        term_black = '#383a42',
+        term_blue = '#0184bc',
+        term_cyan = '#0997b3',
+        term_white = '#eaeaea',
+        term_8 = '#4f525e',
+    }
+end
 
 local pink = '#d291e4'
 local yellow = '#ffcb43'
